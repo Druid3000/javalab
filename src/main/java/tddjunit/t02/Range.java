32803b66e4c214630b3777307e62b40edae00a55
Sergei Sarvarov <sergei_sarvarov@epam.com>
2018-05-31 00:22:49 +0300

    Add interface of Range (tddjuint, task2).

--- src/main/java/tddjunit/t02/Range.java
+++ src/main/java/tddjunit/t02/Range.java
@@ -1,4 +1,23 @@
 package tddjunit.t02;
 
+import java.util.Iterator;
+import java.util.List;
+
 public interface Range {
+
+    boolean isBefore(Range otherRange);
+
+    boolean isAfter(Range otherRange);
+
+    boolean isConcurrent(Range otherRange);
+
+    long getLowerBound();
+
+    long getUpperBound();
+
+    boolean contains(long value);
+
+    List<Long> asList();
+
+    Iterator<Long> asIterator();
 }

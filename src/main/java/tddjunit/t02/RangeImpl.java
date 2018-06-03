3b0d28e742b14aed9fc8eda3ea022397ff302a11
Sergei Sarvarov <sergei_sarvarov@epam.com>
2018-06-03 14:27:56 +0300

    Add test for class RangeImplTest(tddjuint, task2).

---
+++ src/main/java/tddjunit/t02/RangeImpl.java
@@ -0,0 +1,85 @@
+package tddjunit.t02;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class RangeImpl implements Range {
+    private long iteratorPosition;
+    private long lowerBound;
+    private long upperBound;
+
+    RangeImpl(long lowerBound, long upperBound) {
+        if (lowerBound > upperBound) {
+            this.lowerBound = upperBound;
+            this.upperBound = lowerBound;
+        } else {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+        }
+    }
+
+    @Override
+    public long getLowerBound() {
+        return lowerBound;
+    }
+
+    @Override
+    public long getUpperBound() {
+        return upperBound;
+    }
+
+    @Override
+    public boolean isBefore(Range otherRange) {
+        return this.lowerBound> otherRange.getUpperBound();
+    }
+
+    @Override
+    public boolean isAfter(Range otherRange) {
+        return otherRange.getLowerBound()>this.upperBound;
+    }
+
+    @Override
+    public boolean isConcurrent(Range otherRange) {
+        return this.lowerBound <= otherRange.getUpperBound()
+                &&
+                this.upperBound >= otherRange.getLowerBound();
+    }
+
+    @Override
+    public boolean contains(long value) {
+        return value >= lowerBound && value <= upperBound;
+    }
+
+    @Override
+    public List<Long> asList() {
+        List<Long> list;
+        list = new ArrayList<>();
+
+        for (Long i = lowerBound; i < upperBound; i++) {
+            list.add(i);
+        }
+
+        return list;
+    }
+
+    @Override
+    public Iterator<Long> asIterator() {
+        iteratorPosition = lowerBound;
+        return iterator;
+    }
+
+    private Iterator<Long> iterator = new Iterator<Long>() {
+        @Override
+        public Long next() {
+            if (iteratorPosition > upperBound) throw new NoSuchElementException();
+            return iteratorPosition++;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iteratorPosition < upperBound;
+        }
+    };
+}
--- src/test/java/tddjunit/t02/RangeImplTest.java
+++ src/test/java/tddjunit/t02/RangeImplTest.java
@@ -1,4 +1,58 @@
 package tddjunit.t02;
 
-public class RangeImplTest {
+import org.junit.jupiter.api.*;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.hamcrest.core.Is.isA;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class RangeImplTest {
+    private Range range;
+
+    @BeforeEach
+    void setUp() {
+        range = new RangeImpl(1,10);
+    }
+
+    @Test
+    void testAsListNotNull() {
+        assertThat(range.asList(), notNullValue());
+    }
+
+    @Test
+    void testAsListListClass() {
+        assertThat(range.asList(), isA(List.class));
+    }
+
+    @Test
+    void testIsBefore(){
+        Range after = new RangeImpl(-10, -1);
+        assertTrue(range.isBefore(after));
+    }
+
+    @Test
+    void testIsAfter() {
+        Range after = new RangeImpl(11, 20);
+        assertTrue(range.isAfter(after));
+    }
+
+    @Test
+    void testIsContains() {
+        assertFalse(range.contains(15));
+    }
+
+    @Test
+    void testIsConcurrent() {
+        Range concurrent = new RangeImpl(1,10);
+        assertThat(range.isConcurrent(concurrent), is(true));
+    }
+
+    @Test
+    void testAsIterator() {
+        assertThat(range.asIterator().hasNext(), is(true));
+    }
 }
